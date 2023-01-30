00dbfdaffb696e94c14739f14545a359f9fe7a9b
expedia56 <108220408+expedia56@users.noreply.github.com>
2023-01-30 12:57:29 +0900

    Create 10998_A×B.java

---
+++ A.hogyun-im/Sprout/10998_A×B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a * b);
+    }
+}
