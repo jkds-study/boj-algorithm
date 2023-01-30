813797aa20b52c3c325691175f363ed5af1015e4
sojung32 <sojung0320@gmail.com>
2023-01-30 17:56:28 +0900

    Sprout 4

---
+++ sojung-Im/Sprout/10951_A+B - 4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            System.out.println(A+B);
+        }
+        scanner.close();
+    }
+}
