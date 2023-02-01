8b918603298609d8ab08aad882aef71fe7b7642d
expedia56 <108220408+expedia56@users.noreply.github.com>
2023-02-01 10:37:54 +0900

    Sprout1

---
+++ A.hogyun-im/Sprout/2739_구구단.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i=1;i<=9;i++){
+        System.out.println(N + " * " + i + " = " + N*i);
+        }
+    }
+}
