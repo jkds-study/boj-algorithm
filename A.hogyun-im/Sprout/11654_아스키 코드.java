85c1f0348b99c08623227e3a2dc19235127448e5
expedia56 <108220408+expedia56@users.noreply.github.com>
2023-02-03 16:39:55 +0900

    Sprout4

---
+++ A.hogyun-im/Sprout/11654_아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main { 
+    public static void main(String[] args) { 
+       
+		Scanner scan = new Scanner(System.in);
+		
+		int input = scan.next().charAt(0);
+        
+        System.out.println(input);
+        
+        scan.close();
+	}
+}
