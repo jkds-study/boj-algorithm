efce493d6ff721305f5a2331b1fc07c5d4dad4a2
expedia56 <108220408+expedia56@users.noreply.github.com>
2023-02-07 09:11:54 +0900

    Level1-2

---
+++ A.hogyun-im/Sprout/2439_별 찍기 - 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int num = scan.nextInt();
+        
+        for(int i=0; i<num; i++) { 
+            
+            for(int j=i+1; j<num; j++) {
+            
+                System.out.print(" ");
+                
+                
+            }
+            
+            for(int N=num-i; N<=num; N++) {
+                
+                System.out.print("*");   
+            }            
+                     
+            System.out.println();            
+        }
+    }
+}
