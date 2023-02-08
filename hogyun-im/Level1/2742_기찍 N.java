d365ca82bc96205bad4b9dfaee19a6ac85c4af71
expedia56 <108220408+expedia56@users.noreply.github.com>
2023-02-08 13:00:18 +0900

    Level1

---
+++ hogyun-im/Level1/2742_기찍 N.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int N=sc.nextInt();
+   for(int i=N; i>0; i--){
+       System.out.println(i);
+   }
+   sc.close();
+   }       
+}
